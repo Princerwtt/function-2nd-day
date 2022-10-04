16801b8beafc42c2368c488334e5aa64ac92ac3f
Princerwtt <109853314+Princerwtt@users.noreply.github.com>
2022-10-05 01:57:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# function-2nd-day
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-gehboy)
---
+++ script.js
@@ -0,0 +1,29 @@
+function add(num1, num2) {
+  add = num1 + num2;
+  console.log(add);
+}
+add(25, 75);
+
+function sub(num1, num2, num3) {
+  sub = num1 - num2 - num3;
+  console.log(sub);
+}
+sub(66, 12, 6);
+
+function mul(num1, num2, num3, num4) {
+  mul = num1 * num2 * num3 * num4;
+  console.log(mul);
+}
+mul(22, 6, 25, 9);
+
+function div(num1, num2, num3, num4, num5) {
+  div = num1 / num2 / num3 / num4 / num5;
+  console.log(div);
+}
+div(2620, 2, 1, 10, 4);
+
+function remainder(num1, num2) {
+  remainder = num1 % num2;
+  console.log(remainder);
+}
+remainder(100, 3);
